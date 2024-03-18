0a6c0959863f32de34a2096740483486c10f9273
ievgeniy16 <jokert3@mail.ru>
2024-03-18 21:22:37 +0200

    Add random book

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -4,12 +4,26 @@ import "./BookForm.css";
 import { useDispatch } from "react-redux";
 import { addBook } from "../../redux/books/actionCreators";
 import { v4 as uuidv4 } from "uuid";
+import booksData from "../../data/books.json";
 
 const BookForm = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   // hook useDispatch()
   const dispatch = useDispatch();
+
+  const handleAddRandomBook = () => {
+    const randomIndex = Math.floor(Math.random() * booksData.length);
+    const randomBook = booksData[randomIndex];
+
+    const randomBookWithID = {
+      ...randomBook,
+      id: uuidv4(),
+    };
+
+    // send redux store
+    dispatch(addBook(randomBookWithID));
+  };
 
   // если форма большая, то имеет смысл хранить данные в одном объекте
   // const [formData, setFormData] = useState("");
@@ -54,6 +68,7 @@ const BookForm = () => {
           />
         </div>
         <button type="submit">Add Book</button>
+        <button onClick={handleAddRandomBook}>Add Random</button>
       </form>
     </div>
   );
